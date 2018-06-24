6e3c3191c20767b9bc8e92c029a1be489246de3c
Matthew Brandon <matthewbrandon0104@gmail.com>
2018-06-24 12:31:35 -0700

    Countdown start/stop functioning

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
-let min = 9,
-    sec = 0;
+let workSession = 25,
+    shortBreak = 5,
+    longBreak = 30,
+    
+    min = 9,
+    sec = 5;
 
 function draw() {
     let display = `${min} : ${sec}`;
     document.getElementById('test').textContent = display;
 }
 
-let btnPlay = document.getElementById('play');
-let btnPause = document.getElementById('pause');
-
 function countDown() {
     sec--;
+    if (sec < 0) {
+        sec = 59;
+        min--;
+    }
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
     draw()
 }
 
-let play;
+let play,
+    isPlaying = false;
+
+let btnPlay = document.getElementById('play');
+let btnPause = document.getElementById('pause');
+
 btnPlay.addEventListener('click', function() {
+    if (isPlaying === true) {
+        return;
+    } 
     play = setInterval(countDown, 1000);
+    isPlaying = true;
 });
 
 btnPause.addEventListener('click', function() {
-    clearInterval(play); 
+    clearInterval(play);
+    isPlaying = false;
 });
